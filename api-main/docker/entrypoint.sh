0e54ad4d483c02b062f68eeba64a475d56dd7bfb
Nikita Malyshev <mikita.malyshau@stud.esdc.lt>
2026-01-03 12:57:40 +0300

    chore(api): add containerization and runtime setup

---
+++ api-main/docker/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Running migrations"
+php artisan migrate --force
+
+echo "php artisan package:discover --ansi"
+php artisan package:discover --ansi
+
+echo "Starting supervisor"
+exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
